9350918a50eaa2ea3ea438b53622c2c9f3d2078d
classy2 <mingh.dev@gmail.com>
2018-08-02 13:22:45 -0500

    sda

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine(2143);
+            Console.WriteLine();
         }
         
     }
